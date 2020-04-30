f49da364d8a550e59402ad378377ae04c4e9af09
nessangynn <nguyenh22@spu.edu>
2020-04-30 04:38:35 -0700

    Connect Home + About + Charities together with same css. Haven't added Google CSE, and database for email

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// HOME PAGE
+// Swiper function for home page
+var Swipes = new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
+// ABOUT PAGE
+// Contact us form 
+function formSubmit(){
+    var a = document.forms["myform"]["name"].value;
+    var b = document.forms["myform"]["email"].value;
+    var c = document.forms["myform"]["message"].value;
+
+    if (a == "" | b =="" | c=="") {
+    alert("You need to fill out the box with *");
+    return false;
+  }else{
+        alert("We receive you message!");
+        return true;
+    }
+
+};
